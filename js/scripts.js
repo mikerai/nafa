c9922109878594de77b47d483e4227681580b98b
Miguel Ángel Rodríguez Álvarez Icaza <mike.rai@phiveleven.com>
2015-01-23 12:30:29 -0600

    Fancy box added

--- js/scripts.js
+++ js/scripts.js
@@ -178,3 +178,55 @@ $(document).ready(function () {
 $(function(){
   $('#Container').mixItUp();
 });
+
+//FancyBox
+
+$(document).ready(function() {
+    $("#single_1").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
+
+    $("#single_2").fancybox({
+      openEffect  : 'elastic',
+      closeEffect : 'elastic',
+
+      helpers : {
+        title : {
+          type : 'inside'
+        }
+      }
+    });
+
+    $("#single_3").fancybox({
+      openEffect : 'none',
+      closeEffect : 'none',
+      helpers : {
+        title : {
+          type : 'outside'
+        }
+      }
+    });
+
+    $("#single_4").fancybox({
+      helpers : {
+        title : {
+          type : 'over'
+        }
+      }
+    });
+
+    $(".fancybox").fancybox({
+      openEffect  : 'elastic',
+      closeEffect : 'elastic',
+        helpers : {
+          title : {
+            type : 'inside'
+          }
+        }
+    });
+
+});
