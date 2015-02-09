cfb76dd533fb71c0883dea05a7bd1efb3eb63669
Miguel Angel Rodriguez Alvarez Icaza <mike.rai@phiveleven.com>
2015-02-09 09:50:18 -0600

    Image update

--- verify.php
+++ verify.php
@@ -3,11 +3,11 @@
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$message = $_POST["comment"];
-	$text = "<span style='color:rgb(227, 110, 0)'>De:</span> $name<br><br>
-	         <span style='color:rgb(227, 110, 0)'>Correo electr&oacute;nico:</span> $email<br><br>	 
-	         <span style='color:rgb(227, 110, 0)'>Mensaje: </span><br><br> $message";
+	$text = "<span style='color:rgb(234, 107, 0)'>De:</span> $name<br><br>
+	         <span style='color:rgb(234, 107, 0)'>Correo electr&oacute;nico:</span> $email<br><br>	 
+	         <span style='color:rgb(234, 107, 0)'>Mensaje: </span><br><br> $message";
 	$headers = "MIME-Version: 1.0" . "\r\n"; 
 	$headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
 	$headers .= "From: <$email>" . "\r\n";
-	mail($email_to, "Mensaje recibido en el sitio web de Grupo NAFA", $text, $headers);
+	mail($email_to, "Mensaje enviado mediante el sitio web de Grupo NAFA", $text, $headers);
 ?>
